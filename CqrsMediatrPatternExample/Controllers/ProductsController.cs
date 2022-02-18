076054a14518282b6040ed35062bddeda19aff59
Ifeanyi Shadrach Odom <ifeanyishadrach452@gmail.com>
2022-02-18 03:36:05 +0100

    Commit stages

---
+++ CqrsMediatrPatternExample/Controllers/ProductsController.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CqrsMediatrPatternExample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ProductsController(IMediator mediator) => _mediator = mediator;
+        
+    }
+}
---
+++ CqrsMediatrPatternExample/DataStore/Product.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CqrsMediatrPatternExample.DataStore
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
