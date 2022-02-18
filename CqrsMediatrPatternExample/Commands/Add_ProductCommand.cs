954c3770e11dabdce51578c3236c20a601ec37f1
Ifeanyi Shadrach Odom <ifeanyishadrach452@gmail.com>
2022-02-18 09:08:02 +0100

    Commit staged

---
+++ CqrsMediatrPatternExample/Commands/Add_ProductCommand.cs
@@ -0,0 +1,14 @@
+﻿using CqrsMediatrPatternExample.DataStore;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CqrsMediatrPatternExample.Commands
+{
+    public class Add_ProductCommand
+    {
+        public record AddProductCommand(Product Product) : IRequest<Product>;
+    }
+}
